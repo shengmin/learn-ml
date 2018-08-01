7a3d585dac1d5becb3827bf62671271060816ef1
ShengMin Zhang <shengmin@fb.com>
2018-07-31 17:47:48 -0700

    [week-02] one variable gradient descent

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,8 +13,14 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+sum = 0;
 
+for i = 1:m
+    h = theta' * (X(i,:))';
+    sum = sum + ((h - y(i,:)) ^ 2);
+end
 
+J = sum / (2 * m);
 
 
 % =========================================================================
