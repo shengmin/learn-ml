1e2436129a06b6324823449fd1c8fb929c6b72b0
ShengMin Zhang <shengmin@fb.com>
2018-07-31 20:19:19 -0700

    [week-02] multivariate linear regression

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -16,16 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    
+    d = [];
+    for j = 1:size(X, 2)
+        d(j) =  (X * theta - y)' * X(:,j);
+    end
+    theta = theta - alpha / m * d';
 
     % ============================================================
 
